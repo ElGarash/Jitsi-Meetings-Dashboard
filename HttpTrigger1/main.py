92c1dd9f3f8f6c97ab0c1b9d52288c2f2bd61043
Noor Eldeen Salah <m-2123325228@s-mu.edu.eg>
2021-07-16 23:16:50 +0200

    Implement Git workflow

--- HttpTrigger1/main.py
+++ HttpTrigger1/main.py
@@ -1,6 +1,9 @@
+import os
+import subprocess
 import logging
 import azure.functions as func
 from datetime import datetime
+
 
 from .gpages.models import Label, Participant, Meeting, create_tables
 
@@ -8,14 +11,22 @@ from .gpages.models import Label, Participant, Meeting, create_tables
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info("Python HTTP trigger function processed a request.")
     request_body = req.get_json()
+    os.chdir(".\\HttpTrigger1\\gpages")
+    subprocess.run("git pull")
     insert_into_db(request_body)
+    subprocess.run(f'git commit -am "{datetime.now().strftime("%m/%d/%Y, %H:%M:%S")}"')
+    ACCESS_TOKEN = os.environ["GITHUB_ACCESS_TOKEN"]
+    subprocess.run(
+        f"git remote set-url origin https://nooreldeensalah:{ACCESS_TOKEN}@github.com/ElGarash/Jitsi-Meetings-Dashboard.git"
+    )
+    subprocess.run("git push origin")
     return func.HttpResponse(
-        "Items successfully added to the database", status_code=201
+        "Successfully updated the database and pushed the changes.", status_code=200
     )
 
 
 def insert_into_db(data):
-    create_tables()
+    create_tables()  # Create the tables if not already created.
     meeting_date = datetime.now()
     Meeting(date=meeting_date).insert()
     participants = data.get("participants", [])
