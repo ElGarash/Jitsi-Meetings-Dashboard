4e2b0a19030d1a46e13173a76004edaafae00934
Ahmed Abdulhuy <ahmelhuy@gmail.com>
2021-07-29 13:33:21 +0200

    add authorization to API request

--- js/login.js
+++ js/login.js
@@ -1,6 +1,47 @@
+// The Auth0 client, initialized in configureClient()
 let auth0 = null;
 
-const fetchAuthConfig = () => fetch("auth_config.json");
+// * Starts the authentication flow
+
+const login = async (targetUrl) => {
+    try {
+        console.log("Logging in", targetUrl);
+
+        const options = {
+        redirect_uri: window.location.origin
+        };
+
+        if (targetUrl) {
+        options.appState = { targetUrl };
+        }
+
+        await auth0.loginWithRedirect(options);
+    } catch (err) {
+        console.log("Log in failed", err);
+    }
+};
+
+
+// * Executes the logout flow
+
+const logout = () => {
+    try {
+        console.log("Logging out");
+        auth0.logout({
+        returnTo: window.location.origin
+        });
+    } catch (err) {
+        console.log("Log out failed", err);
+    }
+};
+
+
+// * Retrieves the auth configuration from the server
+
+const fetchAuthConfig = () => fetch("/auth_config.json");
+
+
+// * Initializes the Auth0 client
 
 const configureClient = async () => {
     const response = await fetchAuthConfig();
@@ -8,9 +49,26 @@ const configureClient = async () => {
 
     auth0 = await createAuth0Client({
         domain: config.domain,
-        client_id: config.clientId
+        client_id: config.clientId,
+        audience: config.audience
     });
 };
+
+/**
+ * Checks to see if the user is authenticated. If so, `fn` is executed. Otherwise, the user
+ * is prompted to log in
+ * @param {*} fn The function to execute if the user is logged in
+ */
+const requireAuth = async (fn, targetUrl) => {
+    const isAuthenticated = await auth0.isAuthenticated();
+
+    if (isAuthenticated) {
+        return fn();
+    }
+
+    return login(targetUrl);
+};
+
 
 window.onload = async () => {
     await configureClient();
@@ -23,7 +81,7 @@ window.onload = async () => {
     if(isAuthenticated){
 
         return;
-    }
+    }    
 
     // * NEW - check for the code and state parameters
     const query = window.location.search;
@@ -37,8 +95,8 @@ window.onload = async () => {
         // * use replaceState to redirect the user away and remove the querystring parameter
         window.history.replaceState({}, document.title, "/");
         
-    }
-};
+    }    
+};    
 
 
 const updateUI = async () => {
@@ -46,33 +104,23 @@ const updateUI = async () => {
     const isAuthenticated = await auth0.isAuthenticated();
     
     toggelUI(isAuthenticated);
-};
-
-const login = async () => {
-    await auth0.loginWithRedirect({
-        redirect_uri: window.location.origin
-    });
-};
-
-const logout = () => {
-    auth0.logout({
-        returnTo: window.location.origin
-    });
-};
+};    
 
 
 const toggelUI = (isAuthenticated) => {
     
-    // * toggle logout / login
+    // * Show & hide logout / login
+    
     login_request = document.getElementById("login-request");
     login_btn = document.getElementById("btn-login");
     logout_btn = document.getElementById("btn-logout");
-    
+    call_api_btn = document.getElementById("btn-call-api");
+
     if(isAuthenticated){
-        
-        login_request.style.display = "none";
-        logout_btn.style.display = "block";
         login_btn.style.display = "none";
+        logout_btn.style.display = "block";
+        call_api_btn.style.display = "block";
+        login_request.style.display = "none";
         
         // * initialize the meeting API
         const domain = 'meet.jit.si';
@@ -84,20 +132,52 @@ const toggelUI = (isAuthenticated) => {
                 startWithAudioMuted: false,
                 startWithVideoMuted: true,
                 requireDisplayName: true,
-            },
+            },    
             interfaceConfigOverwrite:
             {
                 SHOW_CHROME_EXTENSION_BANNER: false,
-            }
-        };
+            }    
+        };    
 
         const api = new JitsiMeetExternalAPI(domain, options);
     
     } else {
 
-        login_request.style.display = "block";
         document.getElementById("btn-logout").style.display = "none";
         document.getElementById("btn-login").style.display = "block";
-    }
+        login_request.style.display = "block";
+        call_api_btn.style.display = "none";
+    }    
 
-};
+};    
+
+
+
+const callApi = async () => {
+    try {
+
+        // Get the access token from the Auth0 client
+        const token = await auth0.getTokenSilently();
+        let data = {
+                    participants: ["Noor", "Ahmed"],
+                    labels: ["WebRTC", "Authentication"]
+                };    
+        // Make the call to the API, setting the token        
+        // in the Authorization header
+        const response = await fetch("https://meetingtriggerapp.azurewebsites.net/api/insert?", {
+            method: "POST",
+            headers: {
+                "Content-type": "application/json;charset=UTF-8",
+                "Authorization": `Bearer ${token}`,
+            },    
+            body: JSON.stringify({
+                participants: ['Noor', 'Ahmed'],
+                labels: ['WebRTC', 'Authentication'],
+            }),    
+        });    
+
+    } catch (e) {
+        // Display errors in the console
+        console.error(e);
+    }    
+};    
