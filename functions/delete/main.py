e2d82a5f1e1745d23bf9429bb79bb745091c3980
Noor Eldeen Salah <m-2123325228@s-mu.edu.eg>
2021-08-01 03:45:33 +0200

    Implement delete function

--- functions/delete/main.py
+++ functions/delete/main.py
@@ -1,25 +1,57 @@
-# TODO: TO BE IMPLEMENTED.
-import logging
-
+import os
 import azure.functions as func
+from datetime import datetime
+from github import Github
+
+from ..models import session, database_location, Meeting, Label, Participant, session
+from ..auth import get_token_from_auth_header, verify_decode_jwt, check_permissions, PERMISSION
+
+
+ACCESS_TOKEN = os.environ["GITHUB_ACCESS_TOKEN"]
+REPO_FULL_NAME = "ElGarash/Jitsi-Meetings-Dashboard"
+TARGET_BRANCH = "gh-pages"
+DATE_FORMAT = "%B %d, %Y - %I:%M %p"
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
+    # Authentication and Authorization
+    token, token_err = get_token_from_auth_header(req)
+    if token_err:
+        return func.HttpResponse(token_err.message, status_code=token_err.status_code)
+    payload, payload_err = verify_decode_jwt(token)
+    if payload_err:
+        return func.HttpResponse(payload_err.message, status_code=payload_err.status_code)
+    permissions_err = check_permissions(PERMISSION, payload)
+    if permissions_err:
+        return func.HttpResponse(permissions_err.message, status_code=permissions_err.status_code)
+    # Get database file from GitHub
+    g = Github(ACCESS_TOKEN)
+    repo = g.get_repo(REPO_FULL_NAME)
+    db_file_contents = repo.get_contents("database.db", ref=TARGET_BRANCH)
+    with open(database_location, "wb") as db_file:
+        db_file.write(db_file_contents.decoded_content)
+    # Delete a specific meeting from the database.
+    meeting_id = req.params.get("id")
+    deletion_error = delete_meeting(meeting_id)
+    if deletion_error:
+        return func.HttpResponse(deletion_error['message'], status_code=deletion_error['status_code'])
+    # Read the database after deletion and commit it to GitHub.
+    with open(database_location, "rb") as db_file:
+        updated_content = db_file.read()
+    repo.update_file(
+        path=db_file_contents.path,
+        message=f"Azure at {datetime.now().strftime(DATE_FORMAT)}",
+        content=updated_content,
+        sha=db_file_contents.sha,
+        branch=TARGET_BRANCH,
+    )
 
-    name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
+    return func.HttpResponse("Successfully deleted the meeting", status_code=200)
 
-    if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+
+def delete_meeting(meeting_id):
+    meeting = session.get(Meeting, meeting_id)
+    if meeting is None:
+        return {"message": "Meeting doesn't exist", "status_code": 404}
     else:
-        return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
-        )
+        meeting.delete()
