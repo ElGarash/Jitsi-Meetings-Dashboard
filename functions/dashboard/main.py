0c9b8748d2033c6080f5249ef983f7a2aa26b03f
Noor Eldeen Salah <m-2123325228@s-mu.edu.eg>
2021-08-01 16:01:54 +0200

    Refactoring dashboard/main.py

--- functions/dashboard/main.py
+++ functions/dashboard/main.py
@@ -4,12 +4,14 @@ from typing import Union
 from json import dumps
 from sqlalchemy.exc import SQLAlchemyError
 import azure.functions as func
+from operator import attrgetter
 
 from ..models import Label, Participant, Meeting, create_tables, session
 from ..auth import get_token_from_auth_header, verify_decode_jwt, check_permissions, PERMISSION
 from ..github import clone_db_file, push_db_file
 
 RESOURCE_TO_MODEL_MAPPER = {"meetings": Meeting, "participants": Participant, "labels": Label}
+
 
 def main(request: func.HttpRequest) -> func.HttpResponse:
     # Authentication and Authorization.
@@ -23,99 +25,116 @@ def main(request: func.HttpRequest) -> func.HttpResponse:
     if permissions_err:
         return func.HttpResponse(permissions_err.message, status_code=permissions_err.status_code)
     # Method dispatching.
-    if request.method == 'DELETE':
-        resource = request.route_params.get("resources")
-        resource_id = request.route_params.get("id")
-        db_file_metadata = clone_db_file()
-        deletion_error = delete(resource, resource_id)
-        if deletion_error:
-            return func.HttpResponse(deletion_error['message'], status_code=deletion_error['status_code'])
-        push_db_file(db_file_metadata)
-        return func.HttpResponse("Successfully deleted the resource", status_code=200)
-    
-    elif request.method == 'GET':
-        resource = request.route_params.get("resources")
-        if resource != 'secrets':
-            return func.HttpResponse("Method Not Allowed", status_code=405)
-        else:
-            secret_type = request.params.get("type", None)
-            if secret_type is None:
-                return func.HttpResponse("Bad Request", status_code=400)
-            secret = getenv(secret_type.upper(), None)
-            if secret:
-                return func.HttpResponse(secret, status_code=200)
-            else:
-                return func.HttpResponse("Secret not found", status_code=404)
-            
-        
-    elif request.method == 'PATCH':
-        resource = request.route_params.get("resources")
-        resource_id = request.route_params.get("id")
-        request_body = request.get_json()
-        db_file_metadata = clone_db_file()
-        update_error = update(resource, resource_id, request_body)
-        if update_error:
-            return func.HttpResponse(update_error["message"], status_code=update_error["status_code"])
-        push_db_file(db_file_metadata)
-        return func.HttpResponse("Successfully updated the resource", status_code=200)
-    elif request.method == 'POST': 
-        db_file_metadata = clone_db_file()
-        insertion_error = insert(request.get_json())
-        if insertion_error:
-            return func.HttpResponse(insertion_error["message"], status_code=insertion_error["status_code"])
-        push_db_file(db_file_metadata)
-        return func.HttpResponse("Successfully inserted the meeting's data", status_code=201)
-    
+    if request.method == "DELETE":
+        return delete_dispatcher(request)
+    elif request.method == "GET":
+        return get_dispatcher(request)
+    elif request.method == "PATCH":
+        return patch_dispatcher(request)
+    elif request.method == "POST":
+        return post_dispatcher(request)
     else:
         return func.HttpResponse("Method not allowed", status_code=405)
-    
-    
 
-def insert(request_body) -> Union[dict, None]:
+
+def get_dispatcher(request):
+    resource = request.route_params.get("resources")
+    if resource != "secrets":
+        return func.HttpResponse("Method Not Allowed", status_code=405)
+    else:
+        secret_type = request.params.get("type", None)
+        if secret_type is None:
+            return func.HttpResponse("Bad Request", status_code=400)
+        secret = getenv(secret_type.upper(), None)
+        if secret:
+            return func.HttpResponse(secret, status_code=200)
+        else:
+            return func.HttpResponse("Secret not found", status_code=404)
+            
+def post_dispatcher(request) -> Union[dict, None]:
+    db_file_metadata = clone_db_file()
     create_tables()
-    date_format = "%B %d, %Y - %I:%M %p"
-    current_time_str = datetime.now().strftime(date_format)
-    meeting_date = datetime.strptime(current_time_str, date_format)
-    try:
-        Meeting(meeting_date).insert()
-    except SQLAlchemyError:
-        return {"message": "A meeting with the same date and time already exists", "status_code": 409}
-    inserted_meeting = session.query(Meeting).filter(Meeting.date == meeting_date).first()
-    get_name = lambda model: model.name
-    db_participants = set(map(get_name, session.query(Participant).all()))
-    db_labels = set(map(get_name, session.query(Label).all()))
-    participants = request_body.get("participants", [])
-    labels = request_body.get("labels", [])
-    try:
-        for participant in participants:
-            if participant not in db_participants:
-                inserted_meeting.add_child(Participant(participant))
-            else:
-                participant_instance = session.query(Participant).filter(Participant.name == participant).first()
-                inserted_meeting.add_child(participant_instance)
-        for label in labels:
-            if label not in db_labels:
-                inserted_meeting.add_child(Label(label))
-            else:
-                label_instance = session.query(Label).filter(Label.name == label).first()
-                inserted_meeting.add_child(label_instance)
-    except SQLAlchemyError:
-        return {"message": "Failed to insert data into the database", "status_code": 422}
-
-
-def delete(resource, resource_id) -> Union[dict, None]:
+    resource = request.route_params.get("resources")
+    request_body = request.get_json()
     model = RESOURCE_TO_MODEL_MAPPER[resource]
+    if model == Meeting:
+        date_format = "%B %d, %Y - %I:%M %p"
+        current_time_str = datetime.now().strftime(date_format)
+        meeting_date = datetime.strptime(current_time_str, date_format)
+        try:
+            Meeting(meeting_date).insert()
+        except SQLAlchemyError as e:
+            return func.HttpResponse(str(e), status_code=409)
+        inserted_meeting = session.query(Meeting).filter(Meeting.date == meeting_date).first()
+        get_name = attrgetter("name")
+        db_participants = [get_name(participant) for participant in session.query(Participant).all()]
+        db_labels = [get_name(label) for label in session.query(Label).all()]
+        participants = request_body.get("participants", [])
+        labels = request_body.get("labels", [])
+        try:
+            for participant in participants:
+                if participant not in db_participants:
+                    inserted_meeting.add_child(Participant(participant))
+                else:
+                    participant_instance = session.query(Participant).filter(Participant.name == participant).first()
+                    inserted_meeting.add_child(participant_instance)
+            for label in labels:
+                if label not in db_labels:
+                    inserted_meeting.add_child(Label(label))
+                else:
+                    label_instance = session.query(Label).filter(Label.name == label).first()
+                    inserted_meeting.add_child(label_instance)
+        except SQLAlchemyError as e:
+            return func.HttpResponse(str(e), status_code=422)
+    elif model == Participant:
+        name = request_body.get("name")
+        get_name = attrgetter("name")
+        db_participants = [get_name(participant) for participant in session.query(Participant).all()]
+        if name in db_participants:
+            return func.HttpResponse("Resource already exists", status_code=409)
+        else:
+            try:
+                Participant(name).insert()
+            except SQLAlchemyError as e:
+                return func.HttpResponse(str(e), status_code=422)
+    elif model == Label:
+        name = request_body.get("name")
+        get_name = attrgetter("name")
+        db_labels = [get_name(label) for label in session.query(Label).all()]
+        if name in db_labels:
+            return func.HttpResponse("Resource already exists", status_code=409)
+        else:
+            try:
+                Label(name).insert()
+            except SQLAlchemyError as e:
+                return func.HttpResponse(str(e), status_code=422)
+    push_db_file(db_file_metadata)
+    return func.HttpResponse("Resource successfully inserted", status_code=201)
+
+
+def delete_dispatcher(request) -> Union[dict, None]:
+    resource = request.route_params.get("resources")
+    resource_id = request.route_params.get("id")
+    model = RESOURCE_TO_MODEL_MAPPER[resource]
+    db_file_metadata = clone_db_file()
     resource = session.get(model, resource_id)
     if resource is None:
-        return {"message": "Resource doesn't exist", "status_code": 404}
+        return func.HttpResponse("Resource doesn't exist", status_code=404)
     resource.delete()
-        
-        
-def update(resource, resource_id, request_body) -> Union[dict, None]:
+    push_db_file(db_file_metadata)
+    return func.HttpResponse("Successfully deleted the resource", status_code=200)
+    
+
+
+def patch_dispatcher(request) -> Union[dict, None]:
+    resource = request.route_params.get("resources")
+    resource_id = request.route_params.get("id")
+    request_body = request.get_json()
     model = RESOURCE_TO_MODEL_MAPPER[resource]
+    db_file_metadata = clone_db_file()
     resource = session.get(model, resource_id)
     if resource is None:
-        return {"message": "Resource doesn't exist", "status_code": 404}
+        return func.HttpResponse("Resource doesn't exist", status_code=404)
     try:
         if model == Meeting:
             resource.date = request_body.get("date", resource.date)
@@ -131,9 +150,7 @@ def update(resource, resource_id, request_body) -> Union[dict, None]:
             resource.name = request_body.get("name", resource.name)
             resource.meetings = request_body.get("meetings", resource.meetings)
             resource.update()
-    except SQLAlchemyError:
-        return {"message": "Failed to update the resource due to invalid data format", "status_code": 422}
-    
-    
-        
-    
+    except SQLAlchemyError as e:
+        return func.HttpResponse(str(e), status_code=422)
+    push_db_file(db_file_metadata)
+    return func.HttpResponse("Successfully updated the resource", status_code=200)
--- functions/github/__init__.py
+++ functions/github/__init__.py
@@ -5,16 +5,17 @@ from ..models import database_location
 
 
 TARGET_BRANCH = "gh-pages"
-REPO_FULL_NAME = "ElGarash/Jitsi-Meetings-Dashboard"
+REPO_FULL_NAME = "ElGarash/meetings"
 REPO = Github(environ["GITHUB_ACCESS_TOKEN"]).get_repo(REPO_FULL_NAME)
+
 
 def clone_db_file():
     db_file_metadata = REPO.get_contents("database.db", ref=TARGET_BRANCH)
     with open(database_location, "wb") as db_file:
         db_file.write(db_file_metadata.decoded_content)
     return db_file_metadata
-        
-        
+
+
 def push_db_file(db_file_metadata):
     with open(database_location, "rb") as db_file:
         updated_content = db_file.read()
